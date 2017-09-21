e6f3d445a1c67a80dd967e754cb7b85a9f3c0d7b
vijayk-zt162 <vijayk-zt162@vijayk-zt162.tsi.zohocorpin.com>
2017-09-21 18:55:17 +0530

    agents,departments view all & single done

---
+++ PHP/allAgents.php
@@ -0,0 +1,33 @@
+<?php
+    
+    $auth_token = '59550a0e2b1a864a31bef962363e029f'; //your_auth_token
+    $org_id=652853630; //your_organization_id
+    
+    $headers=array(
+            "Authorization: $auth_token",
+            "orgId: $org_id",
+            "contentType: application/json; charset=utf-8",
+    );
+    
+    $url="https://desk.zoho.com/api/v1/agents";
+    
+    $ch= curl_init($url);
+    curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
+    curl_setopt($ch,CURLOPT_HTTPGET,TRUE);
+    
+    $response= curl_exec($ch);
+    $info= curl_getinfo($ch);
+    
+    if($info['http_code']==200){
+        echo "<h2>Request Successful, Response:</h2> <br>";
+        echo $response;
+    }
+    else{
+        echo "Request not successful. Response code : ".$info['http_code']." <br>";
+        echo "Response : $response";
+    }
+    
+    curl_close($ch);
+    
+?>

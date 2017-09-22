ee55a8b2068ff61a2f3f30dce8954f94f6c07fa0
vijayk-zt162 <vijayk-zt162@vijayk-zt162.tsi.zohocorpin.com>
2017-09-22 16:08:31 +0530

    list all items

---
+++ PYTHON/allComments.py
@@ -0,0 +1,21 @@
+import requests
+
+auth_token="59550a0e2b1a864a31bef962363e029f" #YOUR_AUTH_TOKEN
+org_id="652853630" #YOUR_ORGANISATION_ID
+ticket_id='215666000000074114';
+
+params="sortBy=-commentedTime&limit=15"
+
+headers={
+    "Authorization":auth_token,
+    "orgId":org_id,
+    "contentType": "application/json; charset=utf-8"
+}
+
+request=requests.get('https://desk.zoho.com/api/v1/tickets/'+ticket_id+'/comments?'+params, headers=headers)
+
+if request.status_code == 200:
+    print "Request Successful,Response:"
+    print request.content
+else:
+    print "Request not successful,Response code ",request.status_code," \nResponse : ",request.content;

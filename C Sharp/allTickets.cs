8a7c653be68c0affd65c35450084c788478e50b1
vijayk-zt162 <vijayk-zt162@vijayk-zt162.tsi.zohocorpin.com>
2017-09-26 13:22:13 +0530

    C# all tickets

---
+++ C Sharp/allTickets.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Net;
+using System.Text;
+using System.IO;
+namespace some_name
+{
+	class someclass
+	{
+		public static void Main(string[] args)
+		{
+            
+
+			string auth_token = "59550a0e2b1a864a31bef962363e029f"; // YOUR_AUTH_TOKEN
+			string org_id = "652853630"; //YOUR_ORG_ID
+
+			string options = "sortBy=dueDate&limit=15";
+
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://desk.zoho.com/api/v1/tickets?" + options);
+			request.ContentType = "application/json";
+			request.Method = "GET";
+			request.Headers["Authorization"] = auth_token;
+			request.Headers["orgId"] = org_id;
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+				if ((int)response.StatusCode == 200)
+				{
+					Console.WriteLine("Response Code : " + (int)response.StatusCode);
+					Console.WriteLine(readStream.ReadToEnd());
+					response.Close();
+					readStream.Close();
+				}
+
+            }catch(WebException e){
+                Console.WriteLine("Request failed, Response Code : "+(int)((HttpWebResponse)e.Response).StatusCode + " \nResponse Message : ");
+				var message = new StreamReader(e.Response.GetResponseStream()).ReadToEnd();
+                Console.WriteLine(message);
+            }
+
+
+			
+		}
+	}
+}

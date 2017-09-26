4b640d1505d395594321f97133eb1e771ab0bb80
vijayk-zt162 <vijayk-zt162@vijayk-zt162.tsi.zohocorpin.com>
2017-09-26 15:37:54 +0530

    Create ticket done

---
+++ C Sharp/createTicket.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Net;
+using System.Text;
+using System.IO;
+
+class CreateTicket
+{
+	public static void Main(string[] args)
+	{
+		string auth_token = "59550a0e2b1a864a31bef962363e029f"; // YOUR_AUTH_TOKEN
+		string org_id = "652853630"; //YOUR_ORG_ID
+
+		string ticket_data ="{\"contactId\" : \"215666000000074112\", \"subject\" : \" from C# Welcome to Zoho Support. Youve got a sample Request!\", \"departmentId\" : \"215666000000006907\", \"description\" : \"Hai This is Description\"}";
+
+		HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://desk.zoho.com/api/v1/tickets");
+		request.ContentType = "application/json";
+		request.Method = "POST";
+		request.Headers["Authorization"] = auth_token;
+		request.Headers["orgId"] = org_id;
+
+        byte[] jsondata = Encoding.UTF8.GetBytes(ticket_data);
+        request.ContentLength = jsondata.Length;
+
+        Stream requestStream = request.GetRequestStream();
+        requestStream.Write(jsondata,0,jsondata.Length);
+        requestStream.Close();
+
+		try
+		{
+			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+			Stream receiveStream = response.GetResponseStream();
+			StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+			if ((int)response.StatusCode == 200)
+			{
+				Console.WriteLine("Ticket Successfully created ,\nResponse Code : " + (int)response.StatusCode);
+				Console.WriteLine(readStream.ReadToEnd());
+				response.Close();
+				readStream.Close();
+			}
+
+		}
+		catch (WebException e)
+		{
+			Console.WriteLine("Request failed, Response Code : " + (int)((HttpWebResponse)e.Response).StatusCode + " \nResponse Message : ");
+			var message = new StreamReader(e.Response.GetResponseStream()).ReadToEnd();
+			Console.WriteLine(message);
+		}
+
+
+
+	}
+}
